f7061fd23385f163a445bd9231defbc3f9141868
Kota Kato <peony.btn@gmail.com>
2022-03-22 20:54:28 +0900

    WIP

--- script.js
+++
@@ -1,5 +0,0 @@
-function fixLinksHeight() {
-	const links = document.getElementById("links");
-	links.style.height = links.children[0].contentWindow.document.body.scrollHeight + "px";
-	links.children[0].style.height = links.children[0].contentWindow.document.body.scrollHeight + "px";
-}
